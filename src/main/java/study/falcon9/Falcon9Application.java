69a84b92d6f53f49e87ae2cde8af58dbd3094dc0
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-09 13:29:40 +0900

    프로젝트 생성

---
+++ src/main/java/study/falcon9/Falcon9Application.java
@@ -0,0 +1,13 @@
+package study.falcon9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Falcon9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Falcon9Application.class, args);
+	}
+
+}
---
+++ src/test/java/study/falcon9/Falcon9ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.falcon9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Falcon9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
